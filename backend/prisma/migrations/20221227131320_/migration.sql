53c5c344d8192976d60590023aeadf2dce5796b2
Lucas Gomes <lggomes007@gmail.com>
2023-01-15 16:33:13 -0300

    removendo arquivos desnecessarios

--- backend/prisma/migrations/20221227131320_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `eixo_x` on the `ações` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ações" DROP COLUMN "eixo_x",
-ADD COLUMN     "foi_realizada" BOOLEAN NOT NULL DEFAULT false;
