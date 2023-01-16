5f520dc65e5632d72271d14744d1acbe821840e7
Lucas Gomes <lggomes007@gmail.com>
2023-01-16 18:59:19 -0300

    criando jogador no banco Co-authored-by: pedroblome <joaopedroblome78@gmail.com>

--- jogo/index.js
+++ jogo/index.js
@@ -1,39 +1,29 @@
 var prompt = require("prompt-sync")();
-const { Console } = require("console");
 const db = require("./util/db");
 
 async function jogar() {
   console.clear();
 
-  //const nomeJogador = prompt("Digite o nome do seu jogar: ");
-
   const resCategoriaJogador = await db.query(`SELECT * FROM categoria_jogador`);
   const nomeJogador = prompt("Digite o nome do seu jogador: ")
   console.clear()
-  
-  console.log(nomeJogador+ ", escolha a categoria do seu jogador: ");
+
+  console.log(nomeJogador + ", escolha a categoria do seu jogador: ");
 
   let auxCategoria = 1;
-
-
-
-  
   resCategoriaJogador.rows.map((result) => {
     console.log(
-      auxCategoria + ". " + result.tipo + ". Historia: " + result.historia
+      auxCategoria++ + ". " + result.tipo + ". Historia: " + result.historia
     );
-    
-    auxCategoria++;
   });
 
-  const categoriaJogador = prompt("Digite a categoria do seu jogador: ");
+  const categoriaJogador = Number(prompt("Digite a categoria do seu jogador: "));
 
-  const id_categoria = resCategoriaJogador.rows[categoriaJogador -1].id_categoria;
-  console.log(id_categoria)
-  
-//   const resJogadorCriado  = await db.query(`INSERT INTO jogador(nome, id_categoria_jogador,id_regiao) VALUES(${nomeJogador}, )`)
-//   console.log(categoriaJogador)
-
+  const id_categoria_jogador = resCategoriaJogador.rows[categoriaJogador - 1].id_categoria_jogador;
+  await db.query(`INSERT INTO jogador(nome, id_categoria_jogador,id_regiao) 
+    VALUES('${nomeJogador}', '${id_categoria_jogador}', 'c7bedb33-9534-4956-bc4b-cb5f7c2260da')`)
+    .then(() => console.log("Jogador Criado com sucesso!"))
+    .catch(() => console.log("Erro ao criar jogador!"));
 }
 
 async function conectarBanco() {
