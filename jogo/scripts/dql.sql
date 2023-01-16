8c633aebdca79884c27fe5c2ad895933e3d70459
Lucas Gomes <lggomes007@gmail.com>
2023-01-16 19:00:59 -0300

    adicinando queries no dql

---
+++ jogo/scripts/dql.sql
@@ -0,0 +1,3 @@
+-- Funcao Jogar
+SELECT * FROM categoria_jogador
+INSERT INTO jogador(nome, id_categoria_jogador,id_regiao) VALUES('${nomeJogador}', '${id_categoria_jogador}', 'c7bedb33-9534-4956-bc4b-cb5f7c2260da')
