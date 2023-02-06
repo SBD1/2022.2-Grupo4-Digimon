26cf5a84873c52c3bf26d61691c5e898d7661008
pedroblome <joaopedroblome78@gmail.com>
2023-02-06 01:34:20 -0300

    verificando missao antes de pegar

--- jogo/util/missao.js
+++ jogo/util/missao.js
@@ -31,53 +31,76 @@ async function interageMissao(jogadorAtualziado, npc) {
 
     if (opcao == '1') {
         try {
+            //verifica se ja fez essa missao.
+                const resultCheckMissao =  await checkMissaoFeita(missao, jogadorAtualziado)
+                if(resultCheckMissao.lenght === 0){
+                    console.log("missao nova")
+                }
             // cadastra essa missao ao usuario
-            const resMissao = await db.query(`select * from missao where id_npc = '${npc.id_npc}'`);
-            await insertMissaoJogador(resMissao.rows[0], jogadorAtualziado);
-            const resDigivice = await digivice.getDigivice(jogadorAtualziado);
-
-            const resInstDigimon = await digivice.getInstanciaDigimon(resDigivice);
-
-            let auxDigimon = 1;
-            for (const instDigimon of resInstDigimon) {
-                console.log(`${auxDigimon++}. ${instDigimon.nome} - nivel: ${instDigimon.nivel} - vida: ${instDigimon.vida} 
-                - defesa: ${instDigimon.defesa} - ataque: ${instDigimon.ataque} - velocidade: ${instDigimon.velocidade}`);
+                const resMissao = await db.query(`select * from missao where id_npc = '${npc.id_npc}'`);
+                await insertMissaoJogador(resMissao.rows[0], jogadorAtualziado);
+                const resDigivice = await digivice.getDigivice(jogadorAtualziado);
+    
+                const resInstDigimon = await digivice.getInstanciaDigimon(resDigivice);
+    
+                let auxDigimon = 1;
+                for (const instDigimon of resInstDigimon) {
+                    console.log(`${auxDigimon++}. ${instDigimon.nome} - nivel: ${instDigimon.nivel} - vida: ${instDigimon.vida} 
+                    - defesa: ${instDigimon.defesa} - ataque: ${instDigimon.ataque} - velocidade: ${instDigimon.velocidade}`);
+                }
+    
+                const opcao = prompt("Digite o numero do digimon para batalhar: "); // opcao digitada no terminal
+                await batalha.criaBatalha(resInstDigimon[Number(opcao) - 1]);
+                await concluiMissaoJogador(resMissao.rows[0], jogadorAtualziado);
+                
+    
+            } catch (error) {
+                console.log(error.stack)
+                console.log("erro ao aceitar missao");
             }
-
-            const opcao = prompt("Digite o numero do digimon para batalhar: "); // opcao digitada no terminal
-            await batalha.criaBatalha(resInstDigimon[Number(opcao) - 1]);
-
-        } catch (error) {
-            console.log(error.stack)
-            console.log("erro ao aceitar missao");
         }
     }
-}
-
-async function getMissao(npc) {
-    try {
-        const resMissao = await db.query(`select * from missao where id_npc = '${npc.id_npc}'`);
-        return resMissao.rows[0];
-    } catch (error) {
-        console.log(error.stack);
+    
+    async function getMissao(npc) {
+        try {
+            const resMissao = await db.query(`select * from missao where id_npc = '${npc.id_npc}'`);
+            return resMissao.rows[0];
+        } catch (error) {
+            console.log(error.stack);
+        }
     }
-}
 
-async function getMissaoJogador(missao, jogadorAtualziado) {
-    try {
-        const resMissaoJogador = await db.query(`select * from missao_jogador where id_missao = '${missao.id_missao}' and id_jogador = '${jogadorAtualziado.id_jogador}'`);
-        return resMissaoJogador.rows[0];
-    } catch (error) {
-        console.log(error.stack);
+    async function checkMissaoFeita(missao, jogadorAtualziado) {
+        try {
+            const checkMissaoFeita = await db.query(`select * from missao_jogador  where id_missao  = '${missao.id_missao}' and id_jogador = '${jogadorAtualziado.id_jogador}'`)
+            console.log(checkMissaoFeita.rows[0])
+            return checkMissaoFeita.rows[0];
+        } catch (error) {
+            console.log(error.stack);
+        }
     }
-}
-
-async function insertMissaoJogador(missao, jogadorAtualziado) {
-    try {
-        await db.query(`insert into missao_jogador(id_missao, id_jogador, concluida) values('${missao.id_missao}', '${jogadorAtualziado.id_jogador}', false)`);
-    } catch (error) {
-        console.log(error.stack);
+    async function getMissaoJogador(missao, jogadorAtualziado) {
+        try {
+            const resMissaoJogador = await db.query(`select * from missao_jogador where id_missao = '${missao.id_missao}' and id_jogador = '${jogadorAtualziado.id_jogador}'`);
+            return resMissaoJogador.rows[0];
+        } catch (error) {
+            console.log(error.stack);
+        }
     }
-}
-
-module.exports = { interageMissao };
+    
+    async function insertMissaoJogador(missao, jogadorAtualziado) {
+        try {
+            await db.query(`insert into missao_jogador(id_missao, id_jogador, concluida) values('${missao.id_missao}', '${jogadorAtualziado.id_jogador}', false)`);
+        } catch (error) {
+            console.log(error.stack);
+        }
+    }
+    
+    async function concluiMissaoJogador(missao, jogadorAtualziado) {
+        try {
+            await db.query(`update  missao_jogador set concluida = true where id_missao = '${missao.id_missao} and id_jogador = '${jogadorAtualziado.id_jogador}'`);
+        } catch (error) {
+            console.log(error.stack);
+        }
+    }
+    module.exports = { interageMissao };
